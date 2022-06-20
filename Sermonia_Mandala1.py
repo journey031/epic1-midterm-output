f2cb67d69dbe6b764f308597ec1a073b10a97435
journey031 <99713337+journey031@users.noreply.github.com>
2022-06-20 22:05:52 +0800

    Add files via upload

---
+++ Sermonia_Mandala1.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+journey = Turtle()
+
+for times in range(38):
+    journey.color("#8C499B")
+    journey.speed(10)
+    journey.circle(100)
+    journey.color("#592541")
+    journey.forward(200)
+    journey.left(120)
+    journey.color("#D8326C")
+    journey.forward(100)
+    journey.right(120)
+    journey.left(170)
+    journey.left(20)
+    journey.forward(15)
+
+ 
